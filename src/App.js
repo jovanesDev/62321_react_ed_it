7e21d07fa1dbb2f1858a1044c11003fa6c243588
Hovha <jovanes.web@gmail.com>
2023-03-28 22:06:57 -0300

    petisiones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import { useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
-
+//'https://jsonplaceholder.typicode.com/posts'
 function App() {
+
+  useEffect(() => {
+    
+    const getAllPosts = () => {
+      // Sincronico
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.log(err))
+    }
+
+    const getPost = async (id) => {
+
+      try {
+          const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+          console.log(res)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getAllPosts()
+    getPost(1)
+
+
+  }, [])
+  
+
+
   return (
     <div className="App">
       
